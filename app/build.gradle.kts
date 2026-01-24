24974c21b8172a7171252f938f1ce874dcdc75e8
nehakedar0828 <245682130+nehakedar0828@users.noreply.github.com>
2026-01-24 12:44:12 +0530

    Added Dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,4 +58,5 @@ dependencies {
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
     implementation("androidx.navigation:navigation-compose:2.9.6")
+    implementation("androidx.datastore:datastore-preferences:1.1.1")
 }
